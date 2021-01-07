297946fcc6c9c238fad597fb2dce5427a60f72e2
sjdnsjd <61648074+sjdnsjd@users.noreply.github.com>
2021-01-07 20:20:59 +0800

    Update about.php

--- pages/about.php
+++ pages/about.php
@@ -3,8 +3,87 @@
 <head>
   <link rel="stylesheet" type="text/css" href="style.php/about.scss">
 </head>
+<body style="background-color:#dbdbdb;">
+<div style="margin:55px auto;align-items: center;width:980px;height:1685px;background-color:white;">
+<div style="left:0px;right:0px;background-color:#ff8c62;height:16px;"></div>
+<div style="margin:26px auto;background-color:blue;height:266px;width:956px;margin-bottom:36px;"></div>
+<div style="position:relative;left:-13px;height:86px;width:336px;background-color:#ff8c62;font-size:47px;line-height:1.4em;color:white;font-family:Arial,Helvetica,sans-serif;"><div style="margin:10px 0px 0px 15px;display: inline-block;">Meet the Team</div></div>
+<div style="position:relative;left:-6.5px;height:13px;width:13px;background-color:grey;transform:skewX(45deg);z-index:-1;"></div>
+<div style="display:inline-block;width:33%;">
+<div style="margin: 15px auto; width:254px;height:314px">Image Placeholder</div>
+</div>
+<div style="display:inline-block;width:33%;">
+<div style="margin: 15px auto; width:254px;height:314px">Image Placeholder</div>
+</div>
+<div style="display:inline-block;width:33%;">
+<div style="margin: 15px auto; width:254px;height:314px">Image Placeholder</div>
+</div>
+<br>
+<div style="display:inline-block;width:16%;"></div>
+<div style="display:inline-block;width:33%;">
+<div style="margin: 15px auto; width:254px;height:314px">Image Placeholder</div>
+</div>
+<div style="display:inline-block;width:33%;">
+<div style="margin: 15px auto; width:254px;height:314px">Image Placeholder</div>
+</div>
+<div style="display:inline-block;width:16%;"></div>
+<div style="margin:15px auto; width:574.87px; height:78.67px;text-align:center;">
+<span style="font-family:trend-sans-w00-four,sans-serif;font-size:60px;-webkit-text-stroke: 1px white;-webkit-text-fill-color: #ef8c57;text-shadow:1px 1px #ef8c57, 2px 2px #ffffff, 3px 3px #ef8c57, 4px 4px #fff, 5px 5px #ef8c57, 6px 6px #fff;">Meet The Team</span>
+</div>
+<div style="padding: 15px 0px;">
+<div style="display:inline-block;width:33%;text-align:center;height:350px;top:0px;position:relative;">
+<div><h2 style="font-family:impact,impact-w01-2010,impact-w02-2010,impact-w10-2010,sans-serif;color:blue;">Communications and <br>logistics</h2></div>
+<div style="font-family:din-next-w01-light,din-next-w02-light,din-next-w10-light,sans-serif;font-size:16px;line-height: 1.2em;font-stretch:condensed;">
+<div>Noshin CHOWDHURY</div>
+<div>Ritika VASWANI</div>
+<div>Kashish NANIKRAM</div>
+<div>Akshay RAMCHANDANI</div>
+<div>Jenny CHAN</div>
+<div>Vyapti WADHWANEY</div>
+<div>Winnie PAU</div>
+<div>Kiki LEUNG</div>
+<div>Rika KASUYA</div>
+<div>Ian LAI</div>
+<div>Hayden WONG</div>
+<div>Krishna WAKATI</div>
+<div>Jake O'HANLON</div>
+<div>Tin Chi LO</div>
+</div>
 
-<body>
+</div>
+<div style="display:inline-block;width:33%;text-align:center;height:350px;float:top:0px;position:relative;">
+<div><h2 style="font-family:impact,impact-w01-2010,impact-w02-2010,impact-w10-2010,sans-serif;color:blue;">Best</h2></div>
+<div style="font-family:din-next-w01-light,din-next-w02-light,din-next-w10-light,sans-serif;font-size:16px;line-height: 1.2em;font-stretch:condensed;">
+<div>Jasmine YEUNG</div>
+<div>Kanav MEHTA</div>
+<div>Kay LAU</div>
+<div>Dennis CHOW</div>
+<div>Arthur CHEUNG</div>
+<div>Manun CHAUHAAN</div>
+<div>Nikhil RAMCHANDANI</div><br><br><br><br><br><br><br>
+</div>
+
+</div>
+<div style="display:inline-block;width:33%;text-align:center;height:350px;">
+<div><h2 style="font-family:impact,impact-w01-2010,impact-w02-2010,impact-w10-2010,sans-serif;color:blue;">Best</h2></div>
+<div style="font-family:din-next-w01-light,din-next-w02-light,din-next-w10-light,sans-serif;font-size:16px;line-height: 1.2em;font-stretch:condensed;">
+<div>Sunwoo JOO</div>
+<div>Harshita PARMAR</div>
+<div>Hiren KHARE</div>
+<div>Jami CHENG</div>
+<div>Kiros CHONG</div>
+<div>Roshita SAJNANI</div>
+<div>Sofia PAYMASTER</div>
+<div>Charis PAO</div>
+<div>Diya SAMTANI</div>
+<div>Mir KALYANI</div>
+<div>Anders LAMM</div>
+<div>Jainam SANGHVI</div><br><br>
+</div>
+</div>
+</div>
+
+</div>
   <!-- content -->
 </body>
 
