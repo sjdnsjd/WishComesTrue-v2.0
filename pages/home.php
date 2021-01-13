affe538a06bfa7da037b29b6808e734842b71de2
SheepYY039 <jasmine.yyyeung@gmail.com>
2021-01-13 13:01:24 +0800

    structured and styled our vision in home page

--- pages/home.php
+++ pages/home.php
@@ -14,49 +14,40 @@
 				<!-- large img gif -->
 				<img class="home__img" src="images/WCT banner.gif" alt="">
 			</div>
+			<hr class="col-md-12 container__line" />
 		</div>
-		<img src="images/Home/temp.png" width=100% height=10px />
-
-
 		<div class="row align-items-center">
-			<div class="col-md-7">
+			<div class="col-md-7 col-10" style="min-height: 16vw;">
 				<div class="subheading">
 					<div class="subheading__text">Our Vision</div>
 					<img class="subheading__img" alt="Pencil"
 						src="https://static.wixstatic.com/media/2d61d965a8fd4e8da44d2a72e0b7ee86.png/v1/fill/w_324,h_324,al_c,q_85,usm_0.66_1.00_0.01/Pencil.webp" />
 				</div>
 			</div>
-			<div class="col-5">
+			<div class="col-md-5 col-9">
 				<div class="subheading__girl" title="">
-					<div data-testid="linkElement" class="subheading__girl--container">
-						<img alt="Happy Indian Girl" class="subheading__girl--img"
-							src="https://static.wixstatic.com/media/5cc7c17262bb44089076051c4a9003a5.jpg/v1/fill/w_514,h_280,al_c,q_80,usm_0.66_1.00_0.01/Happy%20Indian%20Girl.webp" />
-
-					</div>
-				</div>
-				<!-- <img src="images/Home/Home2.png" width="482" height="243" /> -->
-			</div>
-		</div>
-
-		<div class="row align-items-center">
-			<div class="col-md-10">
-				<h4>
-					Our vision for this project is to create a website, where the wishes of <br>minority groups are published to
-					the public allowing volunteers to <br>choose which ones they want to contribute to.
-				</h4>
-			</div>
-			<div class="col-md-12 row">
-				<div class="col-md-10">
-					<h5>
-						Examples of these include helping children who wish for better grades by tutoring.
-					</h5>
-				</div>
-				<div class="col-md-2">
-					<img src="images/Home/Home_image_2.png" alt="">
+					<img alt="Happy Indian Girl" class="subheading__girl--img"
+						src="https://static.wixstatic.com/media/5cc7c17262bb44089076051c4a9003a5.jpg/v1/fill/w_514,h_280,al_c,q_80,usm_0.66_1.00_0.01/Happy%20Indian%20Girl.webp" />
 				</div>
 			</div>
 		</div>
-		<img src="images/Home/temp.png" width=100% height=10px />
+
+		<div class="row align-items-center paragraph justify-content-end">
+			<div class="col-lg-9 col-md-7 ">
+				<p class="paragraph__first">
+					Our vision for this project is to create a website, where the wishes of minority groups are published to
+					the public allowing volunteers to choose which ones they want to contribute to.
+				</p>
+				<p class="paragraph__second">
+					<br />
+					Examples of these include helping children who wish for better grades by tutoring.
+				</p>
+			</div>
+			<div class="col-lg-3 col-md-5 ms-auto">
+				<img src="images/Home/Home_image_2.png" alt="">
+			</div>
+			<hr class="col-md-12 container__line" />
+		</div>
 
 		<div class="row align-items-center justify-content-center">
 			<div class="col-md-12">
